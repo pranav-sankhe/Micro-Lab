f4f6222e3a4fc09a376ff7d31220eaa7faabb552
sabSAThai <pranavsankhe40@gmail.com>
2017-11-06 14:52:41 +0530

    fixed extra space bug

--- project1/trace_invert.py
+++ project1/trace_invert.py
@@ -9,9 +9,11 @@ infile  = open(file_path)
 outfile = open("datapath_trace.txt", "w")
 count = 0 
 for line in infile:
+    line = line.split('\n')[0]
+    print line 
     inputs   =  line.split(' ')[0]
     outputs  =  line.split(' ')[1]
-    comments =  line.split(' ')[2] + ' ' + line.split(' ')[3]  
+    comments =  line.split(' ')[2] + ' ' + line.split(' ')[3] 
 
     count  = count + 1
     write_line = outputs + ' ' + inputs + '  ' + comments + '\n' 
