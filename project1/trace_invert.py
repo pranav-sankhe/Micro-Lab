e2f83a8424abbef927a00e5551da72e916c319b3
Srivatsan Sridhar <ssrivatsan97@gmail.com>
2017-11-07 00:37:52 +0530

    Want to update

--- project1/trace_invert.py
+++ project1/trace_invert.py
@@ -3,14 +3,13 @@ script to invert control path tracefile
 author: Pranav Sankhe [pranavsankhe40@gmail.com, https://sabsathai.github.io/, https://github.com/sabSAThai ]
 '''
 
-file_path = '/home/pranav/Sem5/microLab/MicroprocessorsLab-master/project1/tracefile.txt'  
+file_path = '/home/srivatsan/Micro-Lab/project1/TRACEFILE.txt'  
 
 infile  = open(file_path)
 outfile = open("datapath_trace.txt", "w")
 count = 0 
 for line in infile:
     line = line.split('\n')[0]
-    print line 
     inputs   =  line.split(' ')[0]
     outputs  =  line.split(' ')[1]
     comments =  line.split(' ')[2] + ' ' + line.split(' ')[3] 
